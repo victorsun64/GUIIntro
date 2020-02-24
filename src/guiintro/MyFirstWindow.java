91111b9f31441c2395bba3d28b2faac4ed1045b1
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2020-02-24 18:01:25 +0100

    Button hat eine Funktion

---
+++ src/guiintro/MyFirstWindow.java
@@ -0,0 +1,67 @@
+package guiintro;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.CoolBar;
+import org.eclipse.swt.widgets.ExpandBar;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class MyFirstWindow {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MyFirstWindow window = new MyFirstWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Button btnMyknopf = new Button(shell, SWT.NONE);
+		btnMyknopf.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Knopf wurde gedrückt!!!");
+			}
+		});
+		btnMyknopf.setBounds(64, 98, 75, 25);
+		btnMyknopf.setText("MyKnopf");
+
+	}
+}
